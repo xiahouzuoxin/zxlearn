ccd9fb8e5564d90ee663f1c5d89164760d939ed3
zuoxin.xiahou <xiahouzuoxin@163.com>
2024-03-12 23:06:58 +0800

    fix model

--- minrec/README.md
+++ minrec/README.md
@@ -2,7 +2,7 @@
 
 I mainly used TF for recommendation task in big company, but it should be more efficient of using pytorch for small recommendation tasks in small company.
 
-This dir is trying to train a CTR model using torch, it's just a toy example. Model is easy but the data preprocess pipeline is more complecate for various inputs.
+This dir is trying to train a CTR model using torch, it's just a toy example and want to make everything minimum. Model is easy but the data preprocess pipeline is more complecate for various inputs.
 
 Supported:
 
--- minrec/model.py
+++ minrec/model.py
@@ -47,14 +47,6 @@ class DNN(nn.Module):
             if 'emb_dim' not in f_config:
                 raise ValueError('emb_dim must be specified for sparse features.')
             return nn.Embedding(len(f_config['vocab']), f_config['emb_dim'])
-        elif f_config['type'] == 'dense' and f_config.get('buckets', None) is not None:
-            print(f'==> Create embedding for dense feature {f_config["name"]}')
-            if 'emb_dim' not in f_config:
-                raise ValueError('emb_dim must be specified when buckets exist for dense features.')
-            # why +2:
-            # 1. buckets = [1,2,3] actually has 4 ranges (-inf,1], (1,2], (2,3], (3,+inf) 
-            # 2. nan value will has an independent embedding
-            return nn.Embedding(len(f_config['buckets']) + 2, f_config['emb_dim'])
         else:
             return None
 
@@ -62,27 +54,22 @@ class DNN(nn.Module):
         sparse_inputs = []
         dense_inputs = []
         
-        for k, f_config in enumerate(self.feat_configs):
-            # print(f_config)
-            f_name = f_config['name']
-            f_type = f_config['type']
-            if f_type == 'sparse':
-                _input_feat = input_feats[f_name]
-                # Lookup embeddings for each elements in th list
-                embedded_list = []
-                max_list_length = _input_feat.shape[1]  # Assuming input_feats[k] is a 2D tensor (batch_size, max_list_length)
-                mask = (_input_feat >= 0).float()  
-                _input_feat = _input_feat * mask.long()  # Set padding to 0 for embedding lookup, will be zeroed out after embedding lookup
-                for idx in range(max_list_length):
-                    masked_emb = self.embeddings[f_name](_input_feat[:, idx]) * mask[:, idx].unsqueeze(-1)
-                    embedded_list.append(masked_emb)
-                # sum pooling the embeddings of the list
-                # embedded_tensor = sum(embedded_list)
-                embedded_tensor = torch.stack(embedded_list, dim=1)
-                embedded_tensor = embedded_tensor.sum(dim=1)
-                sparse_inputs.append(embedded_tensor)
-            else:
-                pass
+        # for k, f_config in enumerate(self.feat_configs):
+        for f_name, f_emb in self.embeddings.items():
+            _input_feat = input_feats[f_name]
+            # Lookup embeddings for each elements in th list
+            embedded_list = []
+            max_list_length = _input_feat.shape[1]  # Assuming input_feats[k] is a 2D tensor (batch_size, max_list_length)
+            mask = (_input_feat >= 0).float()  
+            _input_feat = _input_feat * mask.long()  # Set padding to 0 for embedding lookup, will be zeroed out after embedding lookup
+            for idx in range(max_list_length):
+                masked_emb = f_emb(_input_feat[:, idx]) * mask[:, idx].unsqueeze(-1)
+                embedded_list.append(masked_emb)
+            # sum pooling the embeddings of the list
+            # embedded_tensor = sum(embedded_list)
+            embedded_tensor = torch.stack(embedded_list, dim=1)
+            embedded_tensor = embedded_tensor.sum(dim=1)
+            sparse_inputs.append(embedded_tensor)
 
         # print(sparse_inputs[0].shape, sparse_inputs[1].shape, sparse_inputs[2].shape)
         if 'dense_features' in input_feats:
